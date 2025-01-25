2bfcf6f5a49438e5d27b9dd1ba98c30b30bdadb5
OsamaAthar-yt <osamaathar888@gmail.com>
2025-01-26 03:26:52 +0500

    Done by osama

--- Inventory management.cpp
+++ Inventory management.cpp
@@ -51,5 +51,15 @@ void addProduct() {
     inventory.push_back(p);
     cout << "Product added successfully.\n";
 }
+void showInventory() {
+    if (inventory.empty()) {
+        cout << "Inventory is empty.\n";
+        return;
+    }
 
+    cout << "ID\tName\t\tQuantity\tPrice\n";
+    for (int i = 0; i < inventory.size(); i++) {
+        cout << inventory[i].id << "\t" << inventory[i].name<< "\t" << "\t"<<inventory[i].quantity<< "\t\t" << inventory[i].price << "\n";
+    }
+}
 
